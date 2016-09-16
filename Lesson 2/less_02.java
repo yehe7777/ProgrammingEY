1f0c47263c6a9ff34dbb7044ce9eda3cd57392c9
yehe7777 <yehe7777@gmail.com>
2016-09-16 11:00:03 -0700

    lesson 2

---
+++ Lesson 2/less_02.java
@@ -0,0 +1,22 @@
+public class less_02
+{
+	public static void main(String[]args)
+	{
+		int num1=5;
+		int num2=6;
+		System.out.println (num1 * num2);
+		int l=5;
+		int w=6;
+		int h=4;
+		int surArea= l* w * h;
+		System.out.println ("The surface area of the reactangle is" + surArea);
+		String name= "Daniel Hobo";
+		String address= "17th and K Street";
+		String city= "San Diego, California";
+		String zip= "92130";
+		System.out.println (name + "\n" + address + "\n" + city + "\n" + zip);
+		
+		
+		
+	}
+}
