9bd3d6f7117ad3d449b141ce0848f19220021f51
yehe7777 <yehe7777@gmail.com>
2016-09-20 12:08:09 -0700

    bmi

---
+++ Lesson_03/BMI.java
@@ -0,0 +1,16 @@
+import java.util.Scanner; //import Statement
+public class BMI
+{
+	public static void main(String[]args)
+	{
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("This is BMI calculator. Enter your height in inches");
+		double height = keyboard.nextDouble();
+		System.out.println("Enter your weight in pounds");
+		double weight = keyboard.nextDouble();
+		double bmi = weight/height * 703;
+		System.out.print("Your BMI is: " + bmi);
+	
+
+	}
+}
