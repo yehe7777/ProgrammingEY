12c1776218757c3e27226a00d557c764519365f8
yehe7777 <yehe7777@gmail.com>
2016-09-30 11:51:17 -0700

    Updated Files

---
+++ Lesson_01/Art_02.java
@@ -0,0 +1,10 @@
+public class art_02
+{
+	public static void main(String[]args)
+	{
+		System.out.println("Waves");
+		System.out.println("_.~\"~._.~\"~._.~\"~._.~\"~._");
+		System.out.println("] :->");
+		System.out.println("<%%%%%-------->");
+	}
+}
---
+++ Lesson_01/art_01.java
@@ -0,0 +1,10 @@
+public class art_01
+{
+	public static void main(String[]args)
+	{
+		System.out.println("caterpillar");
+		System.out.println("6ooooooo");
+		System.out.println("baby face");
+		System.out.println("-_-");
+	}
+}
