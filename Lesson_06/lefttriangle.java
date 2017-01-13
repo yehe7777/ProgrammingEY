3ded150fcb37e2cd8a1ea1332cfd4cdfe5503751
yeh_ethan <yehe7777@gmail.com>
2017-01-12 22:36:42 -0800

    updated

---
+++ Lesson_06/lefttriangle.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class lefttriangle
+{
+	public static void main (String[]args)
+	{
+		Scanner kb = new Scanner(System.in);
+		System.out.println("Enter a word:");
+		String word = kb.next();
+		for (int i = 0; i < word.length(); i++)
+		{
+			System.out.println(word.substring(i));
+		}
+	}
+}
