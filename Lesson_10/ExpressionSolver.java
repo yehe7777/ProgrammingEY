80f682f47879b17132a9736933ccf6376e4184c7
yeh_ethan <yehe7777@gmail.com>
2017-01-20 10:59:23 -0800

    update

---
+++ Lesson_10/ExpressionSolver.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ExpressionSolver
+{
+	public static void main(String[]args)
+	{
+		Scanner kb = new Scanner (System.in);
+		System.out.println("Enter an equation");
+		String expression = kb.nextLine();
+		ArrayList<String>equation = new ArrayList<>(Array.asList(expression.split("")));
+		
+		int i = 0;
+		while(i < equation.size())
+		{
+			if( i < equation.size() && equation.get(i).equals("+")
+			{
+				equation.set(i,"" + (Integer.)
+			}	
+		}	
+	}
+
+}
