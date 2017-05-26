5bcc5ab15b65796da62133e7a7e6196b0aaceec9
yeh_ethan <yehe7777@gmail.com>
2017-05-26 11:51:07 -0700

    14.1

---
+++ Lesson_14.1/src/Car.java
@@ -0,0 +1,20 @@
+
+public class Car implements Location
+{
+	public final int ID;
+	public double[] location = new double[2];
+	
+	public Car(){
+		ID = (int)(Math.random()*1000000) ;
+	}
+	public int getID(){
+		return ID;
+	}
+	public double[] getLoc(){
+		return location;
+	}
+	public void move(double x, double y){
+		location[0] += x;
+		location[1] += y;
+	}
+}
---
+++ Lesson_14.1/src/Location.java
@@ -0,0 +1,7 @@
+public interface Location
+{
+	int getID();
+	double[] getLoc();
+	void move(double x, double y);
+}
+
